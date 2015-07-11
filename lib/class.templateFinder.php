aa5e21326d831bb0808609e97224f20e5103b88e
mlzyplntsyntk <emre@sarbay.com>
2015-07-11 21:14:46 +0300

    directory adjustements

---
+++ lib/class.templateFinder.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Spinner Template Finder
+ */
+class templateFinder {
+    var $startIndexes = array();
+    var $endIndex = "";
+    var $moveCaretToEndIfNoEndIndex = false;
+    var $searchContent = false;
+            
+    function templateFinder($startIndexes, $endIndex, $moveCaretToEndIfNoEndIndex) 
+    {
+        $this->startIndexes = $startIndexes;
+        $this->endIndex = $endIndex;
+        $this->moveCaretToEndIfNoEndIndex = $moveCaretToEndIfNoEndIndex;
+    }
+}
