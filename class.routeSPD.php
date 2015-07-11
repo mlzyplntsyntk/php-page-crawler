a2b19a0b95c39e48f030d1a75395856ce02fc189
mlzyplntsyntk <emre@sarbay.com>
2015-07-11 21:09:56 +0300

    Result Definition for Sarch Templates

---
+++ class.routeSPD.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Spinner Result Description
+ */
+class routeSPD {
+    var $startIndexes;
+    var $endIndex;
+    var $fieldName;
+    var $sortOrder;
+    var $breakIfNoneFound;    
+}
