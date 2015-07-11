daaa789f873c35b96de95a926cbd5e3db3b052ea
mlzyplntsyntk <emre@sarbay.com>
2015-07-11 21:15:05 +0300

    include helper file

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Autoloads spinner library files
+ */
+
+require_once 'lib/class.routeSPD.php';
+require_once 'lib/class.templateFinder.php';
+require_once 'lib/class.searchTemplate.php';
+require_once 'lib/class.spinner.php';
